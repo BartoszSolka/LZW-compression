19bae5aea04d69831a97cd4bc6227a29ad76fc87
Bartosz Solka <bartosz.solka@gmail.com>
2019-11-18 23:06:39 +0100

    changed main to better show algorithm effects

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
         String content = args[0];
+
+        System.out.println("Oryginalny tekst");
+        System.out.println("Rozmiar: " + content.length() + ", Zawartość: \"" + content + "\"");
+        System.out.println();
+
         List<Integer> compressedContent = LZW.encode(content);
+
+        System.out.println("Po kompresji");
+        System.out.println("Rozmiar: " + compressedContent.size() + ", Zawartość: " + compressedContent);
+        System.out.println();
+
         String decompressedContent = LZW.decode(compressedContent);
 
-        System.out.println(content);
-        System.out.println(compressedContent);
-        System.out.println(decompressedContent);
+        System.out.println("Po dekompresji");
+        System.out.println("Rozmiar: " + decompressedContent.length() + ", Zawartość: \"" + decompressedContent + "\"");
     }
 }
